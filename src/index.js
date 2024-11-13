2befdf005253a250d323454c36a8ef0887d91c86
Jen (Oen) Chen <38259861+jenoen@users.noreply.github.com>
2024-11-12 16:33:59 -0800

    fixed pathways and packages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
-import ReactDOM from "react-dom"
-import App from "../src/App.js"
+import ReactDOM from "react-dom/client"
+import App from "./App.js"
 import "./style.css"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />)
--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,135 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     clean: true,
+//     publicPath: '/',
+//   },
+
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env', '@babel/preset-react'],
+//           },
+//         },
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//       {
+//         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//         type: 'asset/resource',  // This is the new webpack 5 way to handle assets
+//         generator: {
+//           filename: 'images/[name][ext]'
+//         }
+//       }
+//       // {
+//       //   test: /\.(png|jpe?g|gif|svg)$/,  // Handle image files
+//       //   use: [
+//       //     {
+//       //       loader: 'file-loader',
+//       //       options: {
+//       //         name: '[name].[hash:8].[ext]',  // Output filename pattern
+//       //         outputPath: 'images',  // Output directory for images
+//       //       },
+//       //     },
+//       //   ],
+//       // },
+//     ],
+//   },
+
+//   resolve: {
+//     extensions: ['.js', '.jsx'],
+//   },
+
+//   devServer: {
+//     static: [
+//       {directory: path.join(__dirname, 'public'),
+//         publicPath: '/'
+//       },
+//     //   {
+//     //   directory: path.join(__dirname, 'dist'),
+//     //   publicPath: '/'
+//     // }
+//   ],
+//     compress: true,
+//     port: 9000,
+//     open: true,
+//     historyApiFallback: true,
+//   },
+
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './src/index.html',
+//       filename: 'index.html',
+//     }),
+//   ],
+// };
+
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'development',  // Add mode configuration
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
+    filename: 'bundle.js',
+    publicPath: '/'
   },
-
+    // Add performance hints configuration
+    performance: {
+      hints: false,  // Disable performance hints during development
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000
+    },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',  // This is the new webpack 5 way to handle assets
-        generator: {
-          filename: 'images/[name][ext]'
-        }
+        type: 'asset/resource'
       }
-      // {
-      //   test: /\.(png|jpe?g|gif|svg)$/,  // Handle image files
-      //   use: [
-      //     {
-      //       loader: 'file-loader',
-      //       options: {
-      //         name: '[name].[hash:8].[ext]',  // Output filename pattern
-      //         outputPath: 'images',  // Output directory for images
-      //       },
-      //     },
-      //   ],
-      // },
-    ],
+    ]
   },
-
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-
-  devServer: {
-    static: [
-      {directory: path.join(__dirname, 'public'),
-        publicPath: '/'
-      },
-    //   {
-    //   directory: path.join(__dirname, 'dist'),
-    //   publicPath: '/'
-    // }
-  ],
-    compress: true,
-    port: 9000,
-    open: true,
-    historyApiFallback: true,
-  },
-
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
+      template: path.join(__dirname, 'public', 'index.html')
+    })
   ],
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+    open: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
