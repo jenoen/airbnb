6a1b8c97dbc6b19e57fae62b5ff971fc84f9ab6b
Jen (Oen) Chen <38259861+jenoen@users.noreply.github.com>
2024-11-15 11:12:05 -0800

    edited images url

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ export default function Hero (){
     return (
         <section className="hero">
             
-                <img className="hero--photo " src="/images/photo-grid.png" alt="Photo Grid" />
+                <img className="hero--photo " src="../../public/images/photo-grid.png" alt="Photo Grid" />
                 <h1 className="hero--header">Online Experiences</h1>
                 <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts - all without leaving home.</p>
             
